e5020e3d5b1aa40ba7010024e47f916ba7530882
Sam Dev Code <80136246+SamaelMedina28@users.noreply.github.com>
2025-03-28 21:39:32 +0000

    Falta de corregir Inciso C

--- P5/print_hex.asm
+++ P5/print_hex.asm
@@ -1,5 +1,6 @@
 %include "../LIB/pc_iox.inc"
 
+
 section	.text
 
 	global _start       ;must be declared for using gcc
@@ -32,33 +33,18 @@ _start:                     ;tell linker entry point
 	call putchar
 
 ; INCISO C
+	mul bl, 8
+	mov N,bl
+	mov ax,N
+	call pHex_w
+	
 
-	mov al,15
-	call pHex_b	; muestra en pantalla 64
+
 
 	mov al,10	; cambio de linea
 	call putchar
-
-	mov ax,0x12ab 	; presenta en pantalla 12AB
-	call pHex_w
-
-	mov al,10 	; cambio de linea
-	call putchar
-
-	mov eax,1234ABCDh ; presenta en pantalla 1234ABCD
-	call pHex_dw
-
-	mov al,0x0a	  ; cambio de lines 10 decimal o bien 0A hex
-	call putchar
-
-	mov eax,0x76543210 ; presenta en pantalla 76543210
-    call pHex_dw
-
-
-
-	mov al,10       ; cambio de linea
-	call putchar
-
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
 
+section	.data
+N dw 0x000 ,0xa,0 
