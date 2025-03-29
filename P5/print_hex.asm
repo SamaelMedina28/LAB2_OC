7d13d288983b51130f3719a10951e669e2ab1de5
Sam Dev Code <80136246+SamaelMedina28@users.noreply.github.com>
2025-03-29 16:47:20 +0000

    Termino de inciso E y F

--- P5/print_hex.asm
+++ P5/print_hex.asm
@@ -48,8 +48,65 @@ _start:                     ;tell linker entry point
 	ADD word [N], 1
 	mov  ax,[N]
 	call pHex_w
-	
+	mov al,10	; cambio de linea
+	call putchar 
+	mov al,10	; cambio de linea
+	call putchar  
+
 ; INCISO E
+	mov ax, bx
+	mov cl, 0xFF
+	div cl
+	call pHex_w
+
+    mov cl, al ;Guardo el valor de AL para despues imprimirlo   que tiene el cociente
+	mov al,10	; cambio de linea
+	call putchar   ; Imprimo completo ax 
+     
+
+	mov al, ah      ; Imprimo AH que contiene el reciduo
+    call pHex_b   
+
+	mov al,10	; cambio de linea
+	call putchar  
+
+    mov al, cl	;Imprimo AL
+    call pHex_b 
+	mov al,10	; cambio de linea
+	call putchar 
+	mov al,10	; cambio de linea
+	call putchar 
+
+; INCISO F
+
+	add byte [N], ah
+	mov ax, [N]
+	call pHex_w 
+	mov al,10	; cambio de linea
+	call putchar 
+	dec word [N]
+	mov ax, [N]
+	call pHex_w 
+	mov al, 10              ; Salto de línea
+	call putchar
+	
+	pushfd                  ; Push EFLAGS a la pila
+	pop eax                 ; Pop a EAX para manipularlo
+
+	
+	call pHex_dw            ; Imprime el valor de EFLAGS 
+	; Vemos que se activaron 2 banderas, la de paridad P y la de acarreo C
+	; Esto se debe a que en la ultima operacino unicamente usamos los 8 bits menos ignificativos de N al sumarselos con AH, y ya que esta suma supero los bits que tenia disponibles, el carry se activo
+	; Y el de paridad se activo unicamente por que el resultado de la operacion de decrementar N dio un numero par (E)
+	mov al, 10              ; Salto de línea
+	call putchar
+	mov al,10	; cambio de linea
+	call putchar
+
+; INCISO G
+
+	
+
 
 	
 
