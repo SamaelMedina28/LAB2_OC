082354058062cd40dc7ef1df8098a1bddd49372f
Sam Dev Code <80136246+SamaelMedina28@users.noreply.github.com>
2025-04-25 21:52:45 +0000

    Se agregan comentasio y se arreglo un salto de linea

--- P7/p7.asm
+++ P7/p7.asm
@@ -1,95 +1,96 @@
-
-%include "../LIB/pc_iox.inc"  	; incluir declaraciones de procedimiento externos
-								; que se encuentran en la biblioteca libpc_io.a
+%include "../LIB/pc_iox.inc"  	; incluir declaraciones de procedimientos externos
+                                ; que estan en la biblioteca libpc_io.a
 section	.text
-	global _start       ;referencia para inicio de programa
-	
+    global _start       ; referencia para inicio del programa
+    
 _start: ; Direccionamiento directo
 .inicio:
 ; INCISO A
     call getche	; espera a que el usuario presione una tecla
     cmp al, '0'
-    jb .inicio ; Si el valor de al es menor que 0, salta a la etiqueta .inicio
+    jb .inicio ; Si al es menor que 0, regresa a .inicio
     cmp al, '9'
-    ja .inicio ; Si el valor de al es mayor que 9, salta a la etiqueta .inicio
+    ja .inicio ; Si al es mayor que 9, regresa a .inicio
 
     cmp al, '5'
-    jnb .incisoB ; Si el valor de al no es menor que 0, salta a la etiqueta .incisoB
+    jnb .incisoB ; Si al no es menor que 5, brinca a .incisoB
 
     mov al, 10
-    call putchar ; cambio de linea
+    call putchar ; salto de linea
 
-	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+    mov edx, msg		; edx = direccion de la cadena msg
+    call puts			; imprime la cadena msg (terminada en 0)
     
 ; INCISO B
 .incisoB:
     mov al, 10
-    call putchar ; cambio de linea
+    call putchar ; salto de linea
     call getche	; espera a que el usuario presione una tecla
     cmp al, '0'
-    jb .letra ; Si el valor de al es menor que 0, salta a la etiqueta .inicio
+    jb .letra ; Si al es menor que 0, brinca a .letra
     cmp al, '9'
-    ja .letra ; Si el valor de al es mayor que 9, salta a la etiqueta .fin
-    mov edx, msN ; Se imprime el mensaje de que es un número
+    ja .letra ; Si al es mayor que 9, brinca a .letra
+    mov edx, msN ; Mensaje de que es un numero
     call puts			
-    jmp .incisoC ; Salta a la etiqueta .incisoC
+    jmp .incisoC ; Brinca a .incisoC
 .letra:  
     cmp al, 'A'
-    jb .fin ; Si el valor de al es menor que A, salta a la etiqueta .fin
+    jb .fin ; Si al es menor que A, brinca a .fin
     cmp al, 'Z'
-    ja .fin ; Si el valor de al es mayor que Z, salta a la etiqueta .fin
-    mov edx, msL ; Se imprime el mensaje de que es un número
+    ja .fin ; Si al es mayor que Z, brinca a .fin
+    mov edx, msL ; Mensaje de que es una letra
     call puts			
-    jmp .incisoC ; Salta a la etiqueta .incisoC
+    jmp .incisoC ; Brinca a .incisoC
 
     mov al, 10
-    call putchar ; cambio de linea
+    call putchar ; salto de linea
 
-	mov edx, msg		; edx = dirección de la cadena msg
-	call puts			; imprime cadena msg terminada en valor nulo (0)
+    mov edx, msg		; edx = direccion de la cadena msg
+    call puts			; imprime la cadena msg (terminada en 0)
     
     mov al, 10
-    call putchar ; cambio de linea
+    call putchar ; salto de linea
 ; INCISO C
 .incisoC:
     mov cx, 0x05 
 .bucle:
-    mov al, '*'		; edx = dirección de la cadena msg
-    call putchar			; imprime cadena msg terminada en valor nulo (0)
-    loop .bucle ; decrementa cx y salta a la etiqueta .inicioC si cx no es cero
+    mov al, '*'		; imprime un asterisco
+    call putchar			
+    loop .bucle ; decrementa cx y regresa a .bucle si cx no es cero
 ;INCISO D
     mov ecx, 0  
     mov al, 10
-    call putchar ; cambio de linea
+    call putchar ; salto de linea
 .captura:
-    call getche          ; Leer un carácter del teclado
-    mov [buffer + ecx], al ; Almacenar el carácter en el arreglo usando el índice
-    inc ecx              ; Incrementar el índice
-    cmp ecx, 10          ; Verificar si ya se capturaron 10 caracteres
-    jb .captura          ; Si no, repetir el bucle
+    call getche          ; lee un caracter del teclado
+    mov [buffer + ecx], al ; guarda el caracter en el arreglo
+    inc ecx              ; incrementa el indice
+    cmp ecx, 10          ; checa si ya se capturaron 10 caracteres
+    jb .captura          ; si no, repite el bucle
+    mov al, 10
+    call putchar         ; imprime salto de linea
 
-    ; Mostrar mensaje de confirmación
-    mov edx, msC  ; Cargar la dirección del mensaje
-    call puts            ; Imprimir el mensaje
+    ; Mensaje de confirmacion
+    mov edx, msC  ; carga la direccion del mensaje
+    call puts            ; imprime el mensaje
 
-    ; Imprimir los caracteres almacenados en el arreglo
-    mov ecx, 0           ; Reiniciar índice en 0
+    ; Imprime los caracteres guardados en el arreglo
+    mov ecx, 0           ; reinicia el indice en 0
 .imprimir:
-    mov al, [buffer + ecx] ; Cargar el carácter desde el arreglo usando el índice
-    call putchar         ; Imprimir el carácter
-    mov al, 10           ; Código ASCII para nueva línea
-    call putchar         ; Imprimir nueva línea
-    inc ecx              ; Incrementar el índice
-    cmp ecx, 10          ; Verificar si ya se imprimieron 10 caracteres
+    mov al, [buffer + ecx] ; carga el caracter desde el arreglo
+    call putchar         ; imprime el caracter
+    mov al, 10           ; salto de linea
+    call putchar         ; imprime salto de linea
+    inc ecx              ; incrementa el indice
+    cmp ecx, 10          ; checa si ya se imprimieron 10 caracteres
     jb .imprimir  
 
 
 .fin:
     mov al, 10
-    call putchar ; cambio de linea
-	mov	eax, 1	    	; seleccionar llamada al sistema para fin de programa
-	int	0x80        	; llamada al sistema - fin de programa
+    call putchar ; salto de linea
+    mov	eax, 1	    	; llamada al sistema para terminar el programa
+    int	0x80        	; llamada al sistema - fin del programa
 
 section	.data
 msg	db  'Es menor',0xa,0 
